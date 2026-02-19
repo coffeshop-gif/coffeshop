6588071caaa3222ea356c58054e1a6b13ac92b22
coffeshop-gif <coffeshopp19@gmail.com>
2026-02-18 22:29:46 -0800

    Create code.js

---
+++ code.js
@@ -0,0 +1,8 @@
+// Smooth scroll navbar
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({ behavior: "smooth" });
+    });
+});
